0fb4ee62d76ef0c9f5602c302002db850748e735
FatimaBartels <bartels.fatima@gmail.com>
2024-07-24 17:01:46 +0200

    updated images on homepage and contact, styled and added js file for the scroll to top page

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Get the element - scroll to top
+let topBtn = document.querySelector(".top-btn");
+
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+window.onscroll = () => (topBtn.style.opacity = window.scrollY > 500 ? 1 : 0);
