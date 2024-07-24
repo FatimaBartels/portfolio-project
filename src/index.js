c218603b0d00df8835ceb7109d450ce2d1d342e5
FatimaBartels <bartels.fatima@gmail.com>
2024-07-25 00:29:44 +0200

    updated color logo and added dark theme

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ let topBtn = document.querySelector(".top-btn");
 topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
 window.onscroll = () => (topBtn.style.opacity = window.scrollY > 500 ? 1 : 0);
+
+// Dark theme function
+
+function changeTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+let themeButton = document.querySelector(".slider");
+themeButton.addEventListener("click", changeTheme);
